9d0a9d7bb4400f1a0610b4e92bc830577db143c3
unknown <prachismitapanda10@gmail.com>
2026-03-11 22:07:20 +0530

    content added

--- 090226/client/src/pages/Landing/Landing.jsx
+++ 090226/client/src/pages/Landing/Landing.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import './Landing.css'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing Page</h1>
+    <>
+      <div id='container'>
+
+        <div id='reg-box'>
+
+          <div className='text'>
+            <h1>Effortless task management</h1>
+            <p>Stay organize and productive with our powerful todo app. Manage your tasks,collaborate with your team, and  never miss a deadline.</p>
+          </div>
+          
+          <div className='btn-container'>
+            <button > <Link to='/register' className='normal'> Register</Link></button>
+            <button > <Link to='/login' className='normal'> Login</Link></button>
+          </div>
+          
+        </div>
+
+        <div className='img'>
+          <img src="./todo-list.png" alt="" />
+        </div>
+
     </div>
+    </>
   )
 }
 
