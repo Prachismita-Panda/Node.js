5c8570bc13bee9fa69ed961fc1f6be44459af6ba
unknown <prachismitapanda10@gmail.com>
2026-03-16 18:04:30 +0530

    content added

--- 150326/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require ("express");
-const { registerController, loginController } = require("../controllers/userControllers");
-const router = express.Router();
-
-router.post("/register",registerController);
-router.post("/login",loginController)
-
-module.exports = router;
